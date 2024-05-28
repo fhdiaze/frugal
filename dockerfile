35e7817c24d6a4ffb61b51b57ad747ba1696cf3a
fhdiaze <fhdiaze@users.noreply.github.com>
2024-05-28 17:59:49 +1200

    Feature/price improve (#3)
    * fix: add templates
    
    * feat: update workdir
    
    * fix: change cmd
    
    * feat: improve design
    
    * fix: remove clean line
    
    * feat: upgrade actions
    
    * feat: update versions
    
    ---------
    
    Co-authored-by: fredy.diaz <fredy.diaz@2degrees.nz>

--- dockerfile
+++ dockerfile
@@ -5,20 +5,19 @@ RUN USER=root cargo new --bin frugal
 WORKDIR /frugal
 COPY ./Cargo.lock ./Cargo.lock
 COPY ./Cargo.toml ./Cargo.toml
-RUN cargo build --release
+RUN cargo build --release & rm src/*.rs
 
-RUN rm src/*.rs
 COPY ./src ./src
 COPY ./templates ./templates
-RUN rm ./target/release/deps/frugal*
 RUN cargo build --release
 
 # Bundle Stage
 FROM rust
 
-ENV FRUGAL_SERVER__PORT=80
-
+WORKDIR /frugal
 COPY --from=build /frugal/target/release/frugal .
 COPY ./assets ./assets
 COPY ./config ./config
-CMD ["./frugal"]
+COPY ./templates ./templates
+ENV FRUGAL_SERVER__PORT=80
+ENTRYPOINT ["./frugal"]
--- src/api/web/cmd.rs
+++ src/api/web/cmd.rs
@@ -1,6 +1,6 @@
 use crate::{
-    cmd::{self, Command},
-    infra::error::AppError,
+  cmd::{self, Command},
+  infra::error::AppError,
 };
 use askama::Template;
 use axum::{response::Html, routing::post, Form, Router};
@@ -9,24 +9,24 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct Run {
-    cmd: String,
+  cmd: String,
 }
 
 #[derive(Template)]
-#[template(path = "output.html")]
+#[template(path = "comps/output.html")]
 struct OutputTemplate {
-    result: String,
+  result: String,
 }
 
 async fn handle_run(Form(run): Form<Run>) -> Result<Html<String>, AppError> {
-    let result =
-        Command::try_parse_from(run.cmd.split(' ')).map_or_else(|x| x.to_string(), cmd::run);
-    let template = OutputTemplate { result };
-    let content = template.render().map_err(AppError::Render)?;
+  let result = Command::try_parse_from(run.cmd.split(' '))
+    .map_or_else(|x| x.to_string(), cmd::run);
+  let template = OutputTemplate { result };
+  let content = template.render().map_err(AppError::Render)?;
 
-    Ok(Html(content))
+  Ok(Html(content))
 }
 
 pub fn route() -> Router {
-    Router::new().route("/cmd.run", post(handle_run))
+  Router::new().route("/cmd.run", post(handle_run))
 }

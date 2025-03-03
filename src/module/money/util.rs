4d78e7b3bf6a770a882d3264dbc42c1c2198c586
fredy.diaz <fredy.diaz@2degrees.nz>
2025-03-03 18:42:36 +1300

    fix: correct workflow

--- src/module/money/util.rs
+++ src/module/money/util.rs
@@ -1,21 +1,33 @@
 use std::ops::{Add, Div, Mul};
 
 #[derive(Debug, Clone, Copy)]
-pub struct Money(i64);
+pub struct Money(Inner);
+type Inner = i64;
 
 impl Money {
   pub fn from_minor(minor_amount: f64) -> Self {
-    Money(minor_amount.round() as i64)
+    Money(minor_amount.round() as Inner)
   }
 
-  pub fn to_major(self) -> f64 {
-    self.0 as f64 / 100.0
+  const fn minor(&self) -> i64 {
+    self.0 % 100
+  }
+
+  const fn major(&self) -> i64 {
+    self.0 / 100
+  }
+
+  pub fn to_major(&self) -> f64 {
+    let major = self.major();
+    let minor = self.minor();
+
+    major as f64 + minor as f64 / 100.0
   }
 
   pub fn from_major(amount: f64) -> Money {
     let rounded = (amount * 100.0).round() / 100.0;
 
-    Money((rounded * 100.0) as i64)
+    Money((rounded * 100.0) as Inner)
   }
 }
 
@@ -23,7 +35,7 @@ impl Mul<f64> for Money {
   type Output = Money;
 
   fn mul(self, rhs: f64) -> Self::Output {
-    Money((self.0 as f64 * rhs).round() as i64)
+    Money((self.0 as f64 * rhs).round() as Inner)
   }
 }
 
@@ -31,7 +43,7 @@ impl Div<f64> for Money {
   type Output = Money;
 
   fn div(self, rhs: f64) -> Self::Output {
-    Money((self.0 as f64 / rhs).round() as i64)
+    Money((self.0 as f64 / rhs).round() as Inner)
   }
 }
 
@@ -58,3 +70,6 @@ impl Add<f64> for Money {
     self + Money::from_major(rhs)
   }
 }
+
+#[cfg(test)]
+mod tests {}
